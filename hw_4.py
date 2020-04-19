9b8bf8b7a9fe9edc5800a4aeceb3b57d21ef4960
ppietrasinski <p.t.pietrasinski@gmail.com>
2020-04-19 12:51:35 +0200

    changes in hw

--- hw_4.py
+++ hw_4.py
@@ -6,7 +6,7 @@ male_fnames = ['James', 'Bob', 'Jan', 'Hans', 'Orestes', 'Saturnin']
 surnames = ['Smith', 'Kowalski', 'Yu', 'Bona', 'Muster', 'Skinner', 'Cox', 'Brick', 'Malina']
 countries = ['Poland', 'United Kingdom', 'Germany', 'France', 'Other']
 
-current_year = datetime.datetime.now().strftime('%Y')
+current_year = int(datetime.datetime.now().strftime('%Y'))
 users_list = []
 
 for i in range(10):
@@ -15,10 +15,11 @@ for i in range(10):
     else:
         name = random.choice(male_fnames)
     lastname = random.choice(surnames)
-    email = f'{name}.{lastname}@example.com'
+    email = f'{name.lower()}.{lastname.lower()}@example.com'
     age = random.randint(5, 45)
     country = random.choice(countries)
     adult = True if age >= 18 else False
+    birth_year = current_year - age
 
     user_dict = {
         'firstname': name,
@@ -26,15 +27,17 @@ for i in range(10):
         'email': email,
         'age': age,
         'country': country,
-        'adult': adult
+        'adult': adult,
+        'birth_year': birth_year
     }
     users_list.append(user_dict)
 
 # print(type(current_year))
 
 for user in users_list:
+
     print(f"Hi! I'm {user['firstname']} {user['lastname']}. "
           f"I come from {user['country']} and I was born "
-          f"in {int(current_year) - user['age']}")
+          f"in {user['birth_year']}")
 
 
